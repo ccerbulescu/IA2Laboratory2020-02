ba0eba1874cb8c8e17c73c9945c53b80b8e590f8
ccerbulescu <ccerbulescu@gmail.com>
2020-04-12 10:01:40 +0300

    First App

---
+++ src/main/java/edu/ro/ucv/dcti/ia2/Application.java
@@ -0,0 +1,10 @@
+package edu.ro.ucv.dcti.ia2;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
